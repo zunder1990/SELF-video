acb12cd4a26e7920ff9b82ddc300c8d321f032b3
Zach Underwood <zunder1990@gmail.com>
2018-04-26 15:07:05 -0400

    1

--- ballroom-A.py
+++ ballroom-A.py
@@ -11,7 +11,7 @@ camurl = 'rtsp://10.60.0.52:7447/5ac91bd5ec2e84e0ab7649cb_0'
 
 ballroom = "BallroomA"
 
-starttimes = ["8:00", "9:15", "13:00", "14:00", "13:47"]
+starttimes = ["8:00", "9:15", "13:00", "14:00", "15:01"]
 
 
 scope = ['https://spreadsheets.google.com/feeds',
@@ -21,12 +21,24 @@ gc = gspread.authorize(credentials)
 gdspreadsheet = gc.open("speakers-list")
 worksheet = gdspreadsheet.worksheet(ballroom)
 
+def start():
+	#worksheet_list = gdspreadsheet.worksheets()
+	#print worksheet_list
+	
+	
+	#currentdata = datetime.now().strftime('%m-%d')
+	#print timestamp
+	checklist()
+
+
+
 #This will check the list to see if the current time matches a time in the list
 def checklist():
 	currenttime = datetime.now().strftime('%H:%M')
 	if currenttime in starttimes:
 			print "current time in list = ", currenttime
 			googlesheetlookup()
+			time.sleep(120)#seconds
 	time.sleep(20)#seconds
 	checklist()
 
@@ -37,31 +49,48 @@ def googlesheetlookup():
 	#print("Found something at R%sC%s" % (cell.row, cell.col))
 	
 	
+	#speakername = worksheet.acell("""B""" + str(cell.row) + """ """).value
+	#talkname = worksheet.acell("""C""" + str(cell.row) + """ """).value
+	#talkdesc = worksheet.acell("""G""" + str(cell.row) + """ """).value
+	#talkdate = worksheet.acell("""E""" + str(cell.row) + """ """).value
+	#talktime = worksheet.acell("""F""" + str(cell.row) + """ """).value
+	talkID = worksheet.acell("""A""" + str(cell.row) + """ """).value
+	#print "Speakers name = ", speakername
+	#print "Talk title = ", talkname
+	#print "Talk Desc = ", talkdesc
+	#print "Talk date = ", talkdate
+	#print "Talk time = ", talktime
+	print "Talk ID = ", talkID
+	#return talkID
+	ffmpegrecord(talkID)
+
+def ffmpegrecord(talkID):
+	print "would have started ffmpeg with file "+ talkID +".mkv"
+	time.sleep(20)#seconds to mock the time for ffmpeg to record
+	ffmpegpost(talkID)
+
+def ffmpegpost(talkID):
+	print "would have done post process with ffmpeg"
+	upload(talkID)
+	
+def upload(talkID):
+	cell = worksheet.find(talkID)
+
+	#print("Found something at R%sC%s" % (cell.row, cell.col))
+	
+	
 	speakername = worksheet.acell("""B""" + str(cell.row) + """ """).value
 	talkname = worksheet.acell("""C""" + str(cell.row) + """ """).value
 	talkdesc = worksheet.acell("""G""" + str(cell.row) + """ """).value
-	talkdate = worksheet.acell("""E""" + str(cell.row) + """ """).value
-	talktime = worksheet.acell("""F""" + str(cell.row) + """ """).value
-	talkID = worksheet.acell("""A""" + str(cell.row) + """ """).value
 	print "Speakers name = ", speakername
 	print "Talk title = ", talkname
 	print "Talk Desc = ", talkdesc
-	print "Talk date = ", talkdate
-	print "Talk time = ", talktime
 	print "Talk ID = ", talkID
-	#return talkID
-	ffmpeg(talkID)
+	youtubetitlefull = "" + speakername +" - " + talkname +""
+	youtubetitlecapped = youtubetitlefull[:99]
+	youtubedesc = " 2018 SouthEast LinuxFest;  " + speakername +"; " + talkname +"; " + talkdesc +" "
+					
+	print "would have ran python upload_video.py --file /home/zach/" + talkID + ".mkv --title " + youtubetitlecapped + " --description " + youtubedesc + " --privacyStatus private"
+	
 
-def ffmpeg(talkID):
-	print "would have started ffmpeg with file "+ talkID +".mkv"
-	
-def start():
-	#worksheet_list = gdspreadsheet.worksheets()
-	#print worksheet_list
-	
-	
-	#currentdata = datetime.now().strftime('%m-%d')
-	#print timestamp
-	checklist()
-	
 start()
